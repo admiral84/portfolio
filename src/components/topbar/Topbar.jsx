0be4111b932703d3e5d0e47e307d13bc19661b56
admiral84 <nasreddinejaouadi@hotmail.com>
2021-12-23 21:46:36 +0100

    deploy my react portfolio

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,13 @@
+import "./topbar.scss"
+
+export default function Topbar() {
+    return (
+        <div className="topbar">
+            <div className="wrapper">
+                <div className="left">
+                    <a href="#intro">something</a>
+                </div>
+            </div>
+        </div>
+    )
+}
