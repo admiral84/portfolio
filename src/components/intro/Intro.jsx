ffcceef03cfa7f3252f8d2734d709bdfa6df5976
admiral84 <nasreddinejaouadi@hotmail.com>
2022-01-18 20:00:51 +0100

    first release

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,34 @@
 import "./intro.scss"
+import {init} from 'ityped'
+import { useEffect, useRef } from "react"
 
 export default function Intro() {
+    const textRef= useRef();
+   useEffect(() => {
+    init(textRef.current , {
+        backSpeed:1500,
+        backSpeed:50,
+       
+         showCursor: false, strings: ['Software Engineer','Web Developer', 'Web Designer' ,'Content Creator'] })
+      
+   }, [])
     return (
         <div className='intro' id="intro">
+            <div className="left">
+                <div className="imageContainer">
+                <img src={process.env.PUBLIC_URL + '/assets/man.png'} />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi there, I'm </h2>
+                    <h1>Nasreddine Jaouadi</h1>
+                    <h3><span ref={textRef}></span></h3>
+                    <a href="#portfolio">
+                        <img src={process.env.PUBLIC_URL + "/assets/down.png"} alt="" />
+                    </a>
+                </div>
+            </div>
             
         </div>
     )
--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,13 +1,38 @@
 import "./topbar.scss"
+import {Person,Mail} from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({menuOpen,setMenuOpen}) {
     return (
-        <div className="topbar">
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro">something</a>
-                </div>
-            </div>
+        <div className={"topbar "+(menuOpen && "active") }>
+        <div className="wrapper">
+        <div className="left">
+        <a href="#intro" className="logo"> Admiral.</a>  
+        <div className="itemContainer">
+            <Person  className="icon"/>
+            <span>+216 93 821 429</span>
+
         </div>
-    )
-}
+        <div className="itemContainer">
+            <Mail className="icon" />
+            <span>nasreddinej@gmail.com</span>
+
+        </div>
+             
+
+        </div>
+        <div className="right">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+                <span className="line1"></span>
+                <span className="line2"></span>
+                <span className="line3"></span>
+
+            </div>
+            
+        </div>
+        
+        
+        </div>
+        </div>
+        )
+    }
+    
